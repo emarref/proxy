e9659cf32db910fd2001bf4213fcc4e3fbfbfdb9
Malcolm Fell <emarref@gmail.com>
2016-11-25 13:55:28 +1300

    Add response logging

--- main.go
+++ main.go
@@ -16,13 +16,39 @@ var hostname = flag.String("hostname", "localhost", "Value of the Host http head
 var scheme = flag.String("scheme", "http", "Use http or https locally")
 var insecure = flag.Bool("insecure", false, "If your local server uses a self-signed cert, set this to true")
 
+var requestLogger *log.Logger
+var responseLogger *log.Logger
+
+type transport struct {
+	http.RoundTripper
+}
+
+func (t *transport) RoundTrip(req *http.Request) (resp *http.Response, err error) {
+	resp, err = t.RoundTripper.RoundTrip(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	responseLog := resp.Status
+
+	if 301 == resp.StatusCode || 302 == resp.StatusCode {
+		responseLog = responseLog + " '" + resp.Request.URL.EscapedPath() + "'"
+	}
+
+	responseLogger.Println(responseLog)
+
+	return resp, nil
+}
+
 func main() {
-	log := log.New(os.Stdout, "[proxy]\t", log.LstdFlags)
+	requestLogger = log.New(os.Stdout, "[proxy]\t<-\t", log.LstdFlags)
+	responseLogger = log.New(os.Stdout, "[proxy]\t->\t", log.LstdFlags)
 
 	flag.Parse()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("[" + r.RemoteAddr + "]\t" + r.Method + " " + r.URL.EscapedPath())
+		requestLogger.Println("[" + r.RemoteAddr + "]\t" + r.Method + " " + r.URL.EscapedPath())
 
 		director := func(req *http.Request) {
 			req.URL.Scheme = *scheme
@@ -34,11 +60,11 @@ func main() {
 			TLSClientConfig: &tls.Config{InsecureSkipVerify: *insecure},
 		}
 
-		proxy := &httputil.ReverseProxy{Director: director, Transport: tr}
+		proxy := &httputil.ReverseProxy{Director: director, Transport: &transport{tr}}
 		proxy.ServeHTTP(w, r)
 	})
 
 	fmt.Println("Listening for external requests on port " + *publicPort)
 	fmt.Println("Forwarding traffic to " + *scheme + "://" + *hostname + "/ on port " + *internalPort)
-	log.Fatal(http.ListenAndServe(":"+*publicPort, nil))
+	requestLogger.Fatal(http.ListenAndServe(":"+*publicPort, nil))
 }
